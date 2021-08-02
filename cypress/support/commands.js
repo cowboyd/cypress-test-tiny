ed81ee7b6994924cada2a4fc4f8736a91e9bacd1
Charles Lowell <cowboyd@frontside.io>
2021-08-02 15:51:34 -0500

    Demonstrate failing test case

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("expect", () => {
+  Cypress.log({
+    displayName: "expectation",
+    message: "expectation registered"
+  });
+})
